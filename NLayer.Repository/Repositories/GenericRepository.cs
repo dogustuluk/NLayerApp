7909277d72a1224989d9c51c5c82911dc12bfdb6
dogustuluk <dogus.tuluk@gmail.com>
2022-04-08 22:00:15 +0300

    genericRepository > Update method has been created

--- NLayer.Repository/Repositories/GenericRepository.cs
+++ NLayer.Repository/Repositories/GenericRepository.cs
@@ -58,7 +58,7 @@ namespace NLayer.Repository.Repositories
 
         public void Update(T entity)
         {
-            throw new NotImplementedException();
+            _dbSet.Update(entity);
         }
 
         public IQueryable<T> Where(Expression<Func<T, bool>> expression)
